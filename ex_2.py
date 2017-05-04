a4b9aeea2f312bde6bb2b835b16c4eff70d89b4e
AlexanderdeI <semionchik.a.g@gmail.com>
2017-05-04 23:07:41 +0300

    Add files via upload

--- ex_2.py
+++ ex_2.py
@@ -1,18 +1,19 @@
 user_input = input('Enter the number: ')
 
-'# 'Проверяем ввод пользователя.
-'# 'Если введено больше одного числа, сообщаем и прекращаем программу.
-if user_input.count(' ') > 0:
-	print('You have entered more than one number, please restart')
-	exit()
+'# Проверяем ввод пользователя.'
+'# Если введено не число или больше одного числа,'
+'# сообщаем и прекращаем программу.'
+if user_input.isnumeric() is not True:
+    print("You haven't entered numbers, please restart")
+    exit()
+elif user_input.count(' ') > 0:
+    print('You have entered more than one number, please restart')
+    exit()
 	
 number = int(user_input)
 
-'# 'Проверяем число на четность или нечетность
+'# Проверяем число на четность или нечетность'
 if number % 2 == 0:
-	print('Number {0} is even'.format(number))
+    print('Number {0} is even'.format(number))
 else:
-	print('Number {0} is uneven'.format(number))
-
-
-
+    print('Number {0} is uneven'.format(number))
---
+++ ex_29.py
@@ -0,0 +1,23 @@
+"# Создаем пустой список куда будем добавлять счет участников"
+score = []
+
+"# Выводим имя участников и их счет. Счет добавляем в score"
+name_1 = (input("Имя первого участника: ")).title()
+points_1 = int(input("Счет первого участника: "))
+score.append(points_1)
+
+name_2 = (input("Имя второго участника: ")).title()
+points_2 = int(input("Счет второго участника: "))
+score.append(points_2)
+
+name_3 = (input("Имя третьего участника: ")).title()
+points_3 = int(input("Счет третьего участника: "))
+score.append(points_3)
+
+"# Если счет игрока максимальный в score, выводим соотв. поздарвление"
+if points_1 == max(score):
+    print(f"Поздравляю {name_1}! Вы победили. Ваш счет:{points_1}")
+elif points_2 == max(score):
+    print(f"Поздравляю {name_2}! Вы победили. Ваш счет:{points_2}")
+else:
+    print(f"Поздравляю {name_3}! Вы победили. Ваш счет:{points_3}")
