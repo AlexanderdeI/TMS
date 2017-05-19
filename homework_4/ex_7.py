41bfeb18cb732f175895cc6ef12526f5c17c62a1
Alexandr Semionchik <semionchik.a.g@gmail.com>
2017-05-19 19:26:21 +0300

    ex_6, ex_7 Done!

--- homework_4/ex_7.py
+++ homework_4/ex_7.py
@@ -4,11 +4,11 @@ def enumerate_(iterable, start=0):
         return False
         exit()
     else:
+        result = []
         for el in iterable:
-            result = (start, el)
-            print(result)
+            tuple_el = (start, el)
+            result.append(tuple_el)
             start += 1
-        else:
-            exit()
+    return result
 
 print(enumerate_(['zero', 'one', 'two', 'three']))
